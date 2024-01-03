72937c1db8f9a026e78bef82c4244f5bbabec301
Diego Oliveira <estefano_costa_@hotmail.com>
2024-01-03 15:32:50 -0300

    Create post

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,26 @@
 import styles from './Createpost.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthValue } from "../../context/AuthContext"
 
-export default function Createpost () {
+export default function Createpost() {
+    const [title, setTitle] = useState();
+    const [image, setImage] = useState();
+    const [body, setBody] = useState();
+    const [tags, setTags] = useState();
+    const [formError, setFormError] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h1>Createpost</h1>
+            <h2>Criar post</h2>
+            <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+            <form onSubmit={handleSubmit}>
+
+            </form>
         </div>
     )
 }
