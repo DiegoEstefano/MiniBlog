550ee3e09d65cb3a10256d1ebbed9b37135904e2
Diego <estefano_costa_@hotmail.com>
2024-01-03 21:57:31 -0300

    add create post

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -15,11 +15,58 @@ export default function Createpost() {
     }
 
     return (
-        <div>
+        <div className={styles.create_post}>
             <h2>Criar post</h2>
             <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
             <form onSubmit={handleSubmit}>
-
+                <label>
+                    <span>Título</span>
+                    <input type="text"
+                        name='title'
+                        required
+                        placeholder='Pense em um bom título...'
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>URL da imagem</span>
+                    <input type="image"
+                        name='Insira uma imagem para o seu post'
+                        required
+                        placeholder='Pense em um bom título...'
+                        onChange={(e) => setImage(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>Conteúdo</span>
+                    <textarea
+                        name='Insira o conteúdo do teu post'
+                        required
+                        placeholder='Pense em um bom título...'
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
+                    />
+                </label>
+                <label>
+                    <span>Tags: </span>
+                    <input type="text"
+                        name='tags'
+                        required
+                        placeholder='Insira as tags separadas por virgula'
+                        onChange={(e) => setTags(e.target.value)}
+                        value={tags}
+                    />
+                </label>
+                <button className="btn">Entrar</button>
+               {/*  {!loading && <button className="btn">Entrar</button>}
+                    <button className="btn" disabled>
+                        Aguarde...
+                    </button>
+                {loading && (
+                )}
+                {error && <p className="error">{error}</p>} */}
             </form>
         </div>
     )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -45,59 +45,59 @@ const Register = () => {
       <p>Crie seu usuário e compartilhe suas histórias</p>
       <form onSubmit={handleSubmit}>
         <div>
-        <label>
-          <span>Nome:</span>
-          <input
-            type="text"
-            name="displayName"
-            required
-            placeholder="Nome do usuário"
-            onChange={(e) => setDisplayName(e.target.value)}
-            value={displayName}
-          />
-        </label>
-        <label>
-          <span>E-mail:</span>
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="E-mail do usuário"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </label>
-        <label>
-          <span>Senha:</span>
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Insira a senha"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </label>
-        <label>
-          <span>Confirmação de senha:</span>
-          <input
-            type="password"
-            name="confirmPassword"
-            required
-            placeholder="Confirme a senha"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-          />
-        </label>
-        {!loading && <button className="btn">Entrar</button>}
-        {loading && (
-          <button className="btn" disabled>
-            Aguarde...
-          </button>
-        )}
+          <label>
+            <span>Nome:</span>
+            <input
+              type="text"
+              name="displayName"
+              required
+              placeholder="Nome do usuário"
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+            />
+          </label>
+          <label>
+            <span>E-mail:</span>
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="E-mail do usuário"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder="Insira a senha"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <label>
+            <span>Confirmação de senha:</span>
+            <input
+              type="password"
+              name="confirmPassword"
+              required
+              placeholder="Confirme a senha"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+            />
+          </label>
+          {!loading && <button className="btn">Entrar</button>}
+          {loading && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
+          {error && <p className="error">{error}</p>}
         </div>
-       
-        {error && <p className="error">{error}</p>}
+
       </form>
     </div>
   );
